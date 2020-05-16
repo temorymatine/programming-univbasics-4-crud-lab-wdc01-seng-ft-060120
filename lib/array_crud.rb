95b22f9a11ee8bee928a0f22415efa8bc41f8040
Matine temory <temorymatine@gmail.com>
2020-05-16 15:54:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  bunny=[1,2,3,4,"array!"]
+  bunny=[1,2,3,4,"arrays!"]
   last = bunny.pop 
 end
 
