39e096388cad75a88c659a2d79a2bdb4f4b5aac8
Matine temory <temorymatine@gmail.com>
2020-05-16 15:54:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,31 +17,31 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  bunny=[1,2,3,4]
+  bunny=[1,2,3,4,"array!"]
   last = bunny.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  bunny=[1,2,3,4]
+  bunny=["wow",2,3,4]
  first =  bunny.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  bunny=[2,2,3,4]
+  bunny=[2,2,"am",4]
   bunny[2]
 end
 
 def retrieve_first_element_from_array(array)
-  bunny=[2,2,3,4]
+  bunny=["wow",2,3,4]
   bunny[0]
 end
 
 def retrieve_last_element_from_array(array)
-  bunny=[2,2,3,4]
+  bunny=[2,2,3,"arrays!"]
   bunny[-1]
 end
 
 def update_element_from_index(array, index_number, element)
   bunny=[2,2,3,4]
-bunny[1]= 6
+bunny[1]= "totally"
 end
